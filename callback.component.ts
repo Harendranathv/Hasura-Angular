0e4a18409c13ab741429199d6fad7ffb08ed658f
NishuGoel <nishu0505@gmail.com>
2018-12-14 03:17:33 +0530

    Create callback.component.ts

---
+++ callback.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'cb',
+  template: `<div class="loading">
+  <img src="assets/loading.svg" alt="loading">
+</div>`
+})
+export class CallbackComponent {
+
+
+}

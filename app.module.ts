5d80a2a070cc36e515afeeba20d858f3f83e0026
NishuGoel <nishu0505@gmail.com>
2018-12-14 03:09:34 +0530

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthService } from './auth.service';
+
+// import { GraphQLModule } from './graph-ql/graph-ql.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
